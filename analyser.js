06ce03699c34fb74f9eacb93e96f7211d0b21c1e
Fezzer <chris.bowles@thescreamingsheep.net>
2022-12-14 13:56:11 +0000

    calculate and output average profit change

--- analyser.js
+++ analyser.js
@@ -1,14 +1,23 @@
 let totalMonths = finances.length;
 let netTotal = 0;
+let deltaTotal = 0;
 
 finances.forEach(month => {
   let profit = month[1];
 
   netTotal += profit;
+
+  if (previousMonthProfit !== undefined) {
+    let profitDelta = profit - previousMonthProfit;
+    deltaTotal += profitDelta;
+  }
+
+  previousMonthProfit = profit;
 });
 
 let output = ["Financial Analysis", "----------------------------"];
 output.push(`Total Months: ${totalMonths}`);
 output.push(`Total: £${netTotal}`);
+output.push(`Average  Change: £${(deltaTotal / (totalMonths - 1)).toFixed(2)}`);
 
 console.log(output.join("\n"));
