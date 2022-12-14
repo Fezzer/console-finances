ac2ef47048fed22b7b2de90866a0335b02d3c1d8
Fezzer <chris.bowles@thescreamingsheep.net>
2022-12-14 13:51:46 +0000

    calculate and output total months

---
+++ analyser.js
@@ -0,0 +1,6 @@
+let totalMonths = finances.length;
+
+let output = ["Financial Analysis", "----------------------------"];
+output.push(`Total Months: ${totalMonths}`);
+
+console.log(output.join("\n"));
