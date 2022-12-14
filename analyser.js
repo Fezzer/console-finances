0c80cacb1e905b18b5d75f6d0fed19c9e2a2e663
Fezzer <chris.bowles@thescreamingsheep.net>
2022-12-14 13:53:26 +0000

    calculate and output net profit

--- analyser.js
+++ analyser.js
@@ -1,6 +1,14 @@
 let totalMonths = finances.length;
+let netTotal = 0;
+
+finances.forEach(month => {
+  let profit = month[1];
+
+  netTotal += profit;
+});
 
 let output = ["Financial Analysis", "----------------------------"];
 output.push(`Total Months: ${totalMonths}`);
+output.push(`Total: £${netTotal}`);
 
 console.log(output.join("\n"));
