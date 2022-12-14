8a81f80408550a89d0af8fc2a787bab28afd6331
Fezzer <chris.bowles@thescreamingsheep.net>
2022-12-14 14:14:11 +0000

    calculate and output largest and smallest monthly profit change

--- analyser.js
+++ analyser.js
@@ -2,8 +2,11 @@ let totalMonths = finances.length;
 let netTotal = 0;
 let deltaTotal = 0;
 let previousMonthProfit;
+let maxProfitDelta;
+let minProfitDelta;
 
 finances.forEach(month => {
+  let date = month[0]
   let profit = month[1];
 
   netTotal += profit;
@@ -11,6 +14,14 @@ finances.forEach(month => {
   if (previousMonthProfit !== undefined) {
     let profitDelta = profit - previousMonthProfit;
     deltaTotal += profitDelta;
+
+    if (profitDelta > maxProfitDelta?.delta || maxProfitDelta === undefined) {
+      maxProfitDelta = {month: date, delta: profitDelta};
+    }
+
+    if (profitDelta < minProfitDelta?.delta || minProfitDelta === undefined) {
+      minProfitDelta = {month: date, delta: profitDelta};
+    }
   }
 
   previousMonthProfit = profit;
@@ -20,5 +31,7 @@ let output = ["Financial Analysis", "----------------------------"];
 output.push(`Total Months: ${totalMonths}`);
 output.push(`Total: £${netTotal}`);
 output.push(`Average  Change: £${(deltaTotal / (totalMonths - 1)).toFixed(2)}`);
+output.push(`Greatest Increase in Profits: ${maxProfitDelta.month} (£${maxProfitDelta.delta})`);
+output.push(`Greatest Decrease in Profits: ${minProfitDelta.month} (£${minProfitDelta.delta})`);
 
 console.log(output.join("\n"));
